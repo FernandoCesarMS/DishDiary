14ab15e24041788a3cba77546b285c1d14e76107
Fernando Cesar <cmnando.silva@gmail.com>
2024-05-29 18:04:04 -0300

    Feat: Configuração de banco de dados

--- src/main/java/org/dishdiary/controllers/ReviewsController.java
+++ src/main/java/org/dishdiary/controllers/ReviewsController.java
@@ -1,16 +1,19 @@
 package org.dishdiary.controllers;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/reviews")
 public class ReviewsController {
-    @RequestMapping(value="/api/reviews", method= RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces= MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> getReviews(){
-        return new ResponseEntity<>("Valor retornado com sucesso", HttpStatus.OK);
+        return ResponseEntity.ok("Valor retornado com sucesso");
+    }
+
+    @PostMapping(produces= MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> postReview(){
+        return ResponseEntity.ok("Valor criado!");
     }
 }
