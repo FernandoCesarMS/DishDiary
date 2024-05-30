906e6227dc548cdf8a99fb19cbafa1a354ffbac7
Fernando Cesar <cmnando.silva@gmail.com>
2024-05-30 15:53:04 -0300

    Feat: FindAll na chamada do get reviews

--- src/main/java/org/dishdiary/controllers/ReviewsController.java
+++ src/main/java/org/dishdiary/controllers/ReviewsController.java
@@ -1,15 +1,25 @@
 package org.dishdiary.controllers;
 
+import org.dishdiary.domain.reviews.Review;
+import org.dishdiary.domain.reviews.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/reviews")
 public class ReviewsController {
+
+    @Autowired
+    private ReviewRepository repository;
+
     @GetMapping(produces= MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> getReviews(){
-        return ResponseEntity.ok("Valor retornado com sucesso");
+    public ResponseEntity<Object> getReviews(){
+        List<Review> allReviews = repository.findAll();
+        return ResponseEntity.ok(allReviews);
     }
 
     @PostMapping(produces= MediaType.APPLICATION_JSON_VALUE)
